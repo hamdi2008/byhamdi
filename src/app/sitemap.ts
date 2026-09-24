c545b6abb0ec2b7788da10cfc7b5d91b171246be
Hamdi Mohamud <hamdi2008@gmail.com>
2026-09-23 19:01:06 -0500

    Point sitemap to byhamdi.com

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.byhamdi.co",
+      url: "https://byhamdi.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
